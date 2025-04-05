564a34df7303250b5f5a3bfd2bc0ea96a1e6b559
sigvebjo <sigve@bjorkedal.org>
2025-04-05 11:56:06 +0200

    Setup basic front end skeleton

---
+++ frontend/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+const tab = ref(0)
+</script>
+
+<template>
+	<v-app>
+		<v-app-bar>
+			<v-tabs>
+				<v-tab>
+					Dogs
+				</v-tab>
+				<v-tab>
+					Favorites
+				</v-tab>
+			</v-tabs>
+		</v-app-bar>
+		<v-main>
+			<v-window>
+				<v-window-item class="page-window">
+
+				</v-window-item>
+				<v-window-item class="page-window">
+
+				</v-window-item>
+			</v-window>
+		</v-main>
+	</v-app>
+</template>
+
+<style scoped lang="scss">
+.page-window {
+	min-height: calc(100vh - var(--v-layout-top));
+	max-width: 728px;
+	margin: auto;
+	padding: 16px 24px;
+}
+</style>
