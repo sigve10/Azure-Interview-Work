635726dfc55a16d58e5e83b268e89dafc3a4b016
sigvebjo <sigve@bjorkedal.org>
2025-04-05 13:05:52 +0200

    Setup cross origin for backend

--- backend/src/main/java/no/ntnu/sigve/dogapi/controllers/UserController.java
+++ backend/src/main/java/no/ntnu/sigve/dogapi/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@CrossOrigin(origins = "${frontend.url}")
 @RequestMapping("/user")
 public class UserController {
 	private UserRepository userRepository;
