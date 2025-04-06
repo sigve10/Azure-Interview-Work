eae116f9389d05b9a663ec7724f7f514e6759c47
sigvebjo <sigve@bjorkedal.org>
2025-04-06 08:36:43 +0200

    Centered log in button

--- frontend/src/components/dogFavorites.vue
+++ frontend/src/components/dogFavorites.vue
@@ -15,14 +15,23 @@ onMounted(attemptGetFavorites)
 
 <template>
 	<div>
-		<div v-if="userStore.email === undefined">
-			<v-btn>
+		<div v-if="userStore.email === undefined" class="login-container">
+			<v-btn color="primary">
 				Login to see your favorites
 				<login-popup activator="parent" @success="attemptGetFavorites" >
 					Please enter your email in order to see your favorites.
 				</login-popup>
 			</v-btn>
 		</div>
-		<dog-grid :dogs="userStore.favorites" />
+		<dog-grid v-else :dogs="userStore.favorites" />
 	</div>
 </template>
+
+<style scoped lang="scss">
+.login-container {
+	height: 80vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+}
+</style>
